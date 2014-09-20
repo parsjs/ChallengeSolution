7c996f6c79430651e38dcb5e0768ad2dffb65909
Alireza <alireza@Alirezas-MacBook-Pro.local>
2014-09-20 13:10:33 +0600

    Initial Commit

---
+++ UITableViewControllerChallengeSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewControllerChallengeSolution
+//
+//  Created by Alireza on 9/20/14.
+//  Copyright (c) 2014 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewControllerChallengeSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewControllerChallengeSolution
+//
+//  Created by Alireza on 9/20/14.
+//  Copyright (c) 2014 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
